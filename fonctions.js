a1a343c1e2514ed4aa352b424084a4138d38541e
tsafacjo <jordanetsafack@yahoo.fr>
2017-07-25 03:26:40 +0200

    cuas

---
+++ fonctions.js
@@ -0,0 +1,24 @@
+
+function animerTexte(){
+
+var texte=document.getElementById("anim") ;
+
+texte.style.top="40px"
+
+
+
+ setTimeout(animerTexte,8000);
+ setTimeout(animerTexteStyle,4000);
+ }
+ 
+ 
+ 
+function animerTexteStyle(){
+
+var texte=document.getElementById("anim") ;
+
+texte.style.top="65px" ;
+}
+								
+								
+								
---
+++ schoolarship.php
@@ -0,0 +1,369 @@
+<!DOCTYPE html>
+<html>
+
+
+<head>
+	<meta name="viewport" content="width=device-width, initialscale=1.0">
+	<link href="bootstrap/css/bootstrap.css" rel="stylesheet">
+	<link href="bootstrap/css/bootstrap-theme.min.css" rel="stylesheet">
+	<link href="css.css" rel="stylesheet">
+</head>
+
+
+<body>
+<div id="barre">
+					
+			<div class="container">
+				<div class="pull-right" id="con">
+					<a class="" data-toggle="modal" href="#connexion"> Connexion</a> |
+						
+										
+					<a class="" data-toggle="modal" href="#sign" id="red" >Sign Up</a> 					
+				</div>	
+						
+			</div>
+												
+		</div>
+					<!--fenetre modale de l'inscription-->	
+					<div class="modal fade" id="sign">
+								<div class="modal-dialog">
+									<div class="modal-content">
+										<div class="modal-header">
+											<button  class="close" data-dismiss="modal">x</button>
+												<h4 class="modal-title">Create Your Account</h4>
+										</div>
+									<div class="modal-body">
+									
+									
+							
+									Ici Un jolie formulaire d'inscription!
+									
+									
+									<div class="modal-footer">
+										<button class="btn btn-danger" data-dismiss="modal">Fermer</button>
+									</div>
+									</div>
+									</div>
+								</div>
+							</div>	
+						<!--fenetre modale de la connexion-->
+							<div class="modal fade" id="connexion">
+								<div class="modal-dialog">
+									<div class="modal-content">
+										<div class="modal-header">
+											<button  class="close" data-dismiss="modal">x</button>
+												<h4 class="modal-title">Connexion</h4>
+										</div>
+									<div class="modal-body">
+									
+														
+										<form class="form-horizontal">
+															<fieldset>
+
+															<!-- Form Name -->
+															<legend></legend>
+
+															<!-- Text input-->
+															<div class="form-group">
+															  <label class="col-md-4 control-label" for="Name">Name :</label>  
+															  <div class="col-md-4">
+															  <input id="Name" name="Name" placeholder="Name" class="form-control input-md" type="text">
+																
+															  </div>
+															</div>
+
+															<!-- Password input-->
+															<div class="form-group">
+															  <label class="col-md-4 control-label" for="passwordinput">Password :</label>
+															  <div class="col-md-4">
+																<input id="passwordinput" name="passwordinput" placeholder="Password" class="form-control input-md" type="password">
+																
+															  </div>
+															</div>
+
+															<!-- Button (Double) -->
+															<div class="form-group">
+															  <label class="col-md-4 control-label" for="button1id"></label>
+															  <div class="col-md-8">
+																<button id="button1id" name="button1id" class="btn btn-info">Connexion</button>
+																<button id="button2id" name="button2id" class="btn btn-danger">Cancel</button>
+															  </div>
+															</div>
+
+															</fieldset>
+										</form>
+													
+									
+									
+									<div class="modal-footer">
+										<button class="btn btn-danger" data-dismiss="modal">Fermer</button>
+									</div>
+									</div>
+									</div>
+								</div>
+							</div>
+						
+		</div>
+		
+		<?php include("entete.php"); ?>
+
+
+		<div class="container-fluid" id="couleur">
+		<div class="container" id="corps">
+			<div class="panel  btn-danger">
+
+			<div class="panel-heading">
+			<h3 class="panel-title">SCHOLARSHIP
+			</h3>
+			</div>
+			</div>
+
+
+
+		
+			<table class="table table-bordered table-striped table-condensed" >
+			<tr class="row"  ><th class="col-sm-3" >CITY </th><th class="col-sm-4">MAJOR</th><th class="col-sm-5">SCHOLARSHIP INFORMATION</th></tr>
+			<tr class="row" ><td class="col-sm-3">Nanning </td><td class="col-sm-4 row "> Non degree: Chinese Language</td><td  class="col-sm-5">Tuition fee after scholarship: 4500RMB/year
+			Dormitory fee after scholarship: 1200RMB/year/double room
+			</td></tr>
+
+
+			 <tr class="row"><td class="col-sm-3"> </td><td class="col-sm-4 row ">BA:  International Economics and Trade, 
+
+			Accounting, 
+			</td><td  class="col-sm-5"> </td></tr>
+
+
+
+
+			<tr class="row" ><td class="col-sm-3">Nanjing </td><td class="col-sm-4 row "> BA: Pharmacy</td><td  class="col-sm-5">Tuition fee after scholarship:6000RMB/year
+			Dormitory fee:4500RMB/year/double room
+			Note: This scholarship is for whole academic years, but the annual evaluation is needed.
+
+			</td></tr>
+			<tr class="row" ><td class="col-sm-3"> </td><td class="col-sm-4 row "> MA: Pharmacology</td><td  class="col-sm-5">
+			Tuition fee after scholarship: 7000RMB/year
+			Dormitory fee:5400RMB/year
+			Note: This scholarship is for whole academic years, but the annual evaluation is needed.
+
+			</td></tr>
+
+			<tr class="row" ><td class="col-sm-3"> </td><td class="col-sm-4 row "> PhD: Pharmacology
+
+			</td><td  class="col-sm-5">
+			Tuition fee after scholarship: 6000RMB/year
+			Dormitory fee:4500RMB/year/double room
+			Note : This scholarship is for whole academic years, but the annual evaluation is needed.
+
+			</td></tr>
+
+
+
+			<tr class="row" >  <td class="col-sm-3">  Nanchang</td><td class="col-sm-4 row "> MA: MBA
+
+			</td><td  class="col-sm-5">Tuition fee after scholarship: 0RMB/year
+			Original Tuition fee: 23000rmb
+			Scholarship duration: 1 year
+			Dormitoryfee:4600/7500RMB/year
+			Stipend: 2000RMB/year
+			Strength of MBA in Nanchang
+			1.Student can get least 1 year’s free tuition fee   
+			2.First year with 2000rmb as stipend
+			3.100% admission rate and easy procedure than other university
+			4.“211” project top university in China
+			5.Only 2 year’s study period
+
+
+			</td></tr>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+			<tr class="row" >  <td class="col-sm-3">  Jingzhou</td><td class="col-sm-4 row "> MA: Mechanical Manufacture and Automation, Mechatronic Engineering, Mechanical Design and Theory,
+			Oil-gas Equipment in Safety Engineering,
+			Fluid machinery and Engineering, Mechanical Engineering, 
+			Petroleum Mining Machinery, 
+			Mechanical Manufacture and 
+
+			</td><td  class="col-sm-5">Tuition fee after scholarship: 0RMB/year
+			Dormitory fee after scholarship: 3000RMB/year
+
+
+			</td></tr>
+
+
+			<tr class="row" >  <td class="col-sm-3"></td><td class="col-sm-4 row "> 
+			PhD: Petroleum Mining Machinery, Horticultural plant resources and utilization, Agricultural Geology, Crop Science, Mineral Resource Prospecting and Exploration,
+			Geophysical and Exploration, Geophysical Log, Geodetection and Information Technology, Oil-gas Field of Applied Chemistry, Oil-gas Well Engineering, Oil Field Development Engineering
+
+			</td><td  class="col-sm-5">
+			</td></tr>
+
+			<tr class="row"> <td class="col-sm-3">Hangzhou</td><td class="col-sm-4 row "> 
+			BA: Japanese Language with Chinese and Japanese Bilingual Orientation, Chinese Language and Literature in the Field of Business, Art Design, Business English, Communication Engineering Bilingual Orientation,Law in the Field of International Law, Logistics Management, Logistics Management, E-commerce, Finance, Business Statistics, Accounting, International Hotel Management, International Business
+			</td><td  class="col-sm-5">
+			Original Tuition fee: 25000RMB/year
+			Tuition fee after scholarship: 0~25000RMB/year
+			Dormitory fee: 400RMB/month
+
+			The scholarship has three levels: Level A: 100% tuition waiver of the first year; 
+			Level B: 70% tuition waiver of the first year; 
+			Level C: 40% tuition waiver of the first year 
+
+			Which level you can get depends on your scores and university, if you 
+			have good scores, you can still apply next year by yourself.
+
+			<br/>Note: This scholarship is for whole academic years, but the annual evaluation is needed. We can only apply first year scholarship for them, for the rest years, they need to apply by themselves.
+
+			<br/>Note: Not accept students from Democratic Republic of the Congo and Somali.
+			PS. We can’t guarantee which level he can get , if his score is too bad, he may also get self financed.
+
+			<br/>
+			Note: Not accept students from Democratic Republic of the Congo and Somali.
+			PS. We can’t guarantee which level he can get , if his score is too bad, he may also get self financed.
+
+			</td></tr>
+
+
+
+
+			<tr class="row" rows="10" >  <td class="col-sm-3"></td><td class="col-sm-4 row "> 
+
+			MA: Asian-African Language and Literature, Linguistics and Applied Linguistics in, History of Particular Subjects in the Field of History of Chinese Culture, Information and Communication Engineering, Finance, International Trade, Statistics, Tourism Management, Science of Business Administration
+			</td><td  class="col-sm-5">
+			</td></tr>
+
+
+
+
+			<tr class="row" >  <td class="col-sm-3"></td><td class="col-sm-4 row "> 
+
+			PhD: Accounting, Food Science, Science of Business Administration
+			</td><td  class="col-sm-5">
+			</td></tr>
+
+			<tr class="row" >  <td class="col-sm-3"></td><td class="col-sm-4 row "> 
+
+			PhD: Accounting, Food Science, Science of Business Administration
+			</td><td  class="col-sm-5">
+			</td></tr>
+
+
+
+			<tr class="row" >  <td class="col-sm-3">Huzhou</td><td class="col-sm-4 row "> 
+
+			BA: International Economy and Trade, Computer Science and Technology, Mechanical and Electronic Engineering, Tourism Management
+			Notes: students from Russia, Kazakhstan, Bangladesh, Pakistan, Congo are not acceptable
+
+			</td><td  class="col-sm-5">
+			Original tuition fee: 15000RMB/year
+			Tuition fee after scholarship: 5000RMB/year
+			Accommodation fee after scholarship: 4960RMB/year
+			Intake: Sep. 2016
+			Deadline: July.2016
+			Scholarship Duration: 4 years
+			Notice: Not accept students from Democratic Republic of the Congo
+
+			Degree: Bachelor
+			Duration: 4 years
+			City: Hubei, Jingzhou
+			Major: Electrical engineering
+			Original tuition fee: 13000RMB/year
+			Tuition fee after scholarship: 3000rmb/year
+			Dormitory fee after scholarship: 3000rmb/year
+			Scholarship duration: First year
+			Notes:  Somali students are not available
+
+			</td></tr>
+
+
+
+
+
+			<tr class="row" >  <td class="col-sm-3">Xuzhou</td><td class="col-sm-4 row "> 
+
+			BA: International Economy and Trade, Computer Science and Technology, Mechanical and Electronic Engineering, Tourism Management
+			Notes: students from Russia, Kazakhstan, Bangladesh, Pakistan, Congo are not acceptable
+			BA: Electrical engineering
+			BA: Nursing 
+			Degree: Bachelor 
+			Intake: Autumn 
+			Language: English 
+			Duration: 4year 
+			Original Tuition Fee: 30000 
+			Dormitory Fee: 4600RMB/year 
+			Scholarship Duration: 1year 
+			City: Xuzhou
+
+			</td><td  class="col-sm-5">
+
+			Notes: This scholarship is yearly evaluated and student can apply every new year.
+			1. Scholarship for New Students (First year at admission)
+			1.1 First-class scholarship: CNY 30000, 3 seats.
+			1.2 Second-class scholarship: CNY 15000, 6 seats.
+			1.3 Third-class scholarship: CNY 10000, 12 seats.
+			2. Scholarship for Undergraduate Students (Second to fourth year)
+			2.1 First-class scholarship: CNY 30000 Top 7% of all students.
+			2.2 Second-class scholarship: CNY 15000 Another 15% of all students.
+			2.3 Third-class scholarship: CNY 10000 Remaining 30% of all students.
+			PS. We can’t guarantee which level he can get , if his score is too bad, he may also get self financed.
+
+
+
+			</td></tr>
+
+
+
+			<tr class="row" >  <td class="col-sm-3"> Xuzhou</td><td class="col-sm-4 row "> 
+
+			MBBS
+			</td><td  class="col-sm-5">
+			Tuition fee 28000rmb/year, Accommodation fee 4600rmb/year, 
+			City : Jiangsu province, Xuzhou
+			Language : English  
+			(age cannot older than 1995)
+			Duration : 6 years
+			Deadline: June 20th
+
+			</td></tr>
+
+
+
+
+			</table>
+
+
+		</div>
+
+</div>
+
+
+<?php include("footer.php"); ?>
+
+</body>
+<script src="fonctions.js">
+
+</script>
+<script>
+animerTexte() ;
+
+</script>
+
+
+
+</html>
